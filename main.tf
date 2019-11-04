c960f6eae683f306e3c57449a17eae80fb6601c5
Tom Deckers <tdeckers@cisco.com>
2019-11-04 17:14:41 +0100

    upgrade instance type for testing

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "HelloCisco"
